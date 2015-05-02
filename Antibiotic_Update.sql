1b2618a81605b1cab3d23dd6cf5983780262a502
Abdul Ravoof <ravoof@gmail.com>
2015-05-02 10:11:37 +0400

    Updated database...

--- Antibiotic_Update.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE Antibiotic ADD antibiotic_order INT DEFAULT NULL;
-ALTER TABLE CytologyGeneralTest ADD cytology_general_source VARCHAR(255) DEFAULT NULL;
-select @ordval := 0;
-update Antibiotic set `antibiotic_order` = 
-        (select @ordval := @ordval + 1) order by antibiotic_name;
